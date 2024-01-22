7b4d7a1fee075200c0f44dc95ef4cc01e19b6663
SlavenSoldo <49765107+SlavenSoldo@users.noreply.github.com>
2024-01-22 15:56:22 +0100

    Add files via upload

---
+++ Projects/backend/routes/users.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql2/promise');
+const config = require('../config'); // Pretpostavljamo da je config u istom direktoriju
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const pool = mysql.createPool(config.db);
+
+router.post('/register', async (req, res, next) => {
+  try {
+    const { imePrezime, imePrezimePartnera, email, lozinka, grad, uloga } =
+      req.body;
+    // Ovdje biste dodali validaciju podataka
+    const aktivan = 1; // Ako želite da novi korisnici budu automatski aktivni
+
+    const hashedPassword = await bcrypt.hash(lozinka, saltRounds);
+
+    const query = `
+      INSERT INTO Korisnici (ImePrezime, ImePrezimePartnera, Email, Lozinka, Uloga, Grad, Aktivan)
+      VALUES (?, ?, ?, ?, ?, ?, ?)
+    `;
+    const [result] = await pool.execute(query, [
+      imePrezime,
+      imePrezimePartnera,
+      email,
+      hashedPassword,
+      uloga,
+      grad,
+      aktivan,
+    ]);
+
+    res.status(201).send({ id: result.insertId });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const { email, lozinka } = req.body;
+    const query = 'SELECT * FROM Korisnici WHERE Email = ?';
+    const [users] = await pool.execute(query, [email]);
+
+    if (users.length === 0) {
+      return res.status(401).json({ message: 'Korisnik nije pronađen.' });
+    }
+
+    const user = users[0];
+
+    const match = await bcrypt.compare(lozinka, user.Lozinka);
+    if (!match) {
+      return res.status(401).json({ message: 'Neispravna lozinka.' });
+    }
+
+    res.json({
+      user: {
+        id: user.IDKorisnika,
+        imePrezime: user.ImePrezime,
+        email: user.Email,
+        uloga: user.Uloga,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/loginGost', async (req, res, next) => {
+  try {
+    const { imePrezime, email, brojPozivnice } = req.body;
+    const uloga = 'Gost';
+
+    let queryParams = [imePrezime, email, uloga, brojPozivnice];
+    let query = `
+      INSERT INTO Korisnici (ImePrezime, Email, Uloga, BrojPozivnice)
+      VALUES (?, ?, ?, ?)
+    `;
+
+    const [result] = await pool.execute(query, queryParams);
+
+    res.json({
+      user: {
+        id: result.insertId,
+        imePrezime: imePrezime,
+        email: email,
+        uloga: uloga,
+        brojPozivnice: brojPozivnice || null,
+      },
+    });
+  } catch (error) {
+    console.error('Error during guest login:', error);
+    res.status(500).json({ message: 'Došlo je do greške pri prijavi gosta.' });
+  }
+});
+module.exports = router;
